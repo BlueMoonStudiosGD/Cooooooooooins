e8f88865691e880f68ec6f44593801e5e2266f83
BlueMoonStudiosGD <bluemoonstudiosgd@gmail.com>
2026-09-09 05:30:21 -0600

    using returnToLastScene to return in LevelSelectLayer
    OH MY GOD I DON'T KNOW IT EXISTS, THANKS!!!!

--- src/hooks/GameManager.cpp
+++ src/hooks/GameManager.cpp
@@ -4,24 +4,42 @@ using namespace geode::prelude;
 
 #include <Geode/modify/GameManager.hpp>
 
-
 class $modify(GameManager) {
-    
-    bool isIconUnlocked(int id, IconType type) {
-        
-        if(Mod::get()->getSettingValue<bool>("all-icons")) {
-            return true;
-        }
-        
-        return GameManager::isIconUnlocked(id,type);
+
+  bool isIconUnlocked(int id, IconType type) {
+
+    if (Mod::get()->getSettingValue<bool>("all-icons")) {
+      return true;
     }
 
-    bool isColorUnlocked(int id, UnlockType type) {
+    return GameManager::isIconUnlocked(id, type);
+  }
 
-        if(Mod::get()->getSettingValue<bool>("all-icons")) {
-            return true;
-        }
-        
-        return GameManager::isColorUnlocked(id,type);
+  bool isColorUnlocked(int id, UnlockType type) {
+
+    if (Mod::get()->getSettingValue<bool>("all-icons")) {
+      return true;
     }
+
+    return GameManager::isColorUnlocked(id, type);
+  }
+
+  void returnToLastScene(GJGameLevel *level) {
+
+    int levelID = PlayLayer::get()->m_level->m_levelID;
+
+    auto director = CCDirector::sharedDirector();
+
+    int mainLevelIDs[10] = {1, 2, 3, 8, 13, 14, 17, 19, 20, 21};
+
+    for (int i = 0; i < 10; i++) {
+      if (mainLevelIDs[i] == levelID) {
+        director->replaceScene(
+            CCTransitionFade::create(0.5f, LevelSelectLayer::scene(i)));
+        break;
+      }
+    }
+
+    GameManager::returnToLastScene(level);
+  }
 };
